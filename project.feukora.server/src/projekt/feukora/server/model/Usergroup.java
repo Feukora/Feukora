335b2efbf1c83c0cf1ab9ffc219e016ed1c5b93f
Tamara <Tamara@Tamara-HP.home>
2016-05-09 20:31:02 +0200

    Tamara: Klasse Usergroup erstellt (nur Model)

--- project.feukora.server/src/projekt/feukora/server/model/Usergroup.java
+++ project.feukora.server/src/projekt/feukora/server/model/Usergroup.java
@@ -1,5 +1,51 @@
 package projekt.feukora.server.model;
 
-public class Usergroup {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+@Entity
+@NamedQueries({
+		@NamedQuery(name = "Usergroup.findByUsergroupid", query = "SELECT ug FROM Usergroup ug WHERE ug.usergroupid=:usergroupid"),
+		@NamedQuery(name = "Usergroup.findByName", query = "SELECT ug FROM Usergroup ug WHERE ug.name=:name") })
+public class Usergroup implements Serializable {
+
+	@Id
+	@GeneratedValue
+	private int usergroupid;
+	private String name;
+
+	
+	public Usergroup() {
+
+	}
+	
+	public Usergroup(int usergroupid, String name) {
+		this.usergroupid = usergroupid;
+		this.name = name;
+		
+	}
+
+	public int getUsergroupid() {
+		return usergroupid;
+	}
+
+	public void setUsergroupid(int usergroupid) {
+		this.usergroupid = usergroupid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
