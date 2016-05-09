d3bec84a8fe703a6b67a9d133fa3d139a03f0767
Allan <Allan@AllanPC>
2016-05-09 13:42:12 +0200

    Allan Blessing:
    Customerfunction und CustomerfunctionPersister + Impl.

--- project.feukora.server/src/projekt/feukora/server/model/Customerfunction.java
+++ project.feukora.server/src/projekt/feukora/server/model/Customerfunction.java
@@ -1,5 +1,62 @@
 package projekt.feukora.server.model;
 
-public class Customerfunction {
+import java.io.Serializable;
+
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+/**
+ * 
+ * @version 1.0
+ * @author Allan
+ *
+ */
+
+// dadurch kommmt es in die DB // embeddable wenn die id als Fremdschlüssel woanders genutzt wird
+@Embeddable
+
+
+// implements dazuschreiben
+public class Customerfunction implements Serializable {
+
+// Alle Attribute reinholen private und gleich schreiben wie im ERD
+	@Id // immer schreiben vor den attribute
+	@GeneratedValue // immer schreiben vor den attribute
+	private int customerfunctionid;
+	private String name;
+	
+	// leeren Konstruktor machen
+	public Customerfunction(){
+	}
+	
+	/**
+	 * 
+	 * @param customerfunctionid
+	 * @param name
+	 */ // konstruktor mit allen Attributen
+	public Customerfunction(int customerfunctionid, String name){
+		this.customerfunctionid = customerfunctionid;
+		this.name = name;
+	}
+	
+	
+	public int getCustomerfunctionid() {
+		return customerfunctionid;
+	}
+	public void setCustomerfunctionid(int customerfunctionid) {
+		this.customerfunctionid = customerfunctionid;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 
 }
