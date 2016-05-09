ea8965dbdb5a60c3288c43a0d1eb3bb07c0191cd
Feukora <sandro-fasser@bluewin.ch>
2016-05-09 15:05:28 +0200

    Sandro Fasser Arbeiten zum RMI feukora.server.rmi

--- project.feukora.server/src/projekt/feukora/server/rmi/RMIServer.java
+++ project.feukora.server/src/projekt/feukora/server/rmi/RMIServer.java
@@ -1,29 +1,113 @@
 package projekt.feukora.server.rmi;
 
+import java.io.InputStream;
+import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Properties;
+
+import javax.swing.JOptionPane;
+
+import org.eclipse.persistence.sessions.server.Server;
+
+/**
+ * 
+ * @version 1.0
+ * @author Sandro Fasser
+ *
+ */
 
 public class RMIServer {
 
+	
 	public static void main(String[] args) {
 		
-		int port = 60140;
-		String hostIP = "1"
 		
+		/* Konfigurationsdaten einlesen */
+		Properties props = new Properties();
+		InputStream is = Server.class.getClassLoader().getResourceAsStream("rmiserver.properties");
+		props.load(is);
+		int port = Integer.parseInt(props.getProperty("server_port"));
+		String hostIP = props.getProperty("server_ip");
+		
+
+		String studentROName = "studentRO";
+		String dozentROName = "dozentRO";
+		String modulROName = "modulRO";
+
+		Registry registry = null;
+
 		try {
-			// Entferntes Objekt erzeugen
-			Adder adder = new AdderImpl();
-			// Entferntes Objekt beim Namensdienst registrieren
-			Naming.rebind("AdderObjekt", adder);
-			// Ausgabe – Server bereit
-			System.out.println("Server ready");
-			} catch (RemoteException re){
-			re.printStackTrace();
-			} catch (MalformedURLException me){
-			me.printStackTrace();
+
+			if (args.length == 1) {
+				// Port auslesen
+				try {
+					port = Integer.parseInt(args[0]);
+				} catch (NumberFormatException e) {
+					System.out.println(e.getMessage());
+					return;
+				}
+			} else if (args.length == 2) {
+
+				// hostIp auslesen
+				hostIP = args[0];
+
+				// Port auslesen
+				try {
+					port = Integer.parseInt(args[1]);
+				} catch (NumberFormatException e) {
+					System.out.println(e.getMessage());
+					return;
+				}
 			}
 
+			InetAddress.getLocalHost();
+
+			/*
+			 * Die NIC-IP nach 'aussen' kommunizieren (falls die Namensauflï¿½sung
+			 * probleme bereiten sollte)
+			 */
+			System.setProperty("java.rmi.server.hostname", hostIP);
+
+			// Registry starten
+			registry = LocateRegistry.createRegistry(port, null, null);
+
+			if (registry != null) {
+
+				// Entfernte Objekte erstellen
+				StudentRO studentRO = new StudentROImpl();
+				DozentRO dozentRO = new DozentROImpl();
+				ModulRO modulRO = new ModulROImpl();
+
+				registry.rebind(studentROName, studentRO);
+				registry.rebind(dozentROName, dozentRO);
+				registry.rebind(modulROName, modulRO);
+
+				String msg = "RMI-Server ist bereit fuer Client-Anfragen.\n\n"
+						+ "Server herunterfahren?";
+				JOptionPane.showMessageDialog(null, msg, "ServerName ["
+						+ hostIP + ":" + port + "]",
+						JOptionPane.QUESTION_MESSAGE);
+
+				registry.unbind(studentROName);
+				registry.unbind(dozentROName);
+				registry.unbind(modulROName);
+
+				System.out.println("RMI Server wird heruntergefahren!\n");
+
+				System.exit(0);
+			} else {
+				System.out
+						.println("Entferntes Objekt konnte nicht exportiert werden!");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+
+
 
 }
