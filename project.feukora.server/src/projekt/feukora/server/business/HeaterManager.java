341de45bacadaa22a6edb1dc71d0a69af47806ed
Perry1234 <perry_00@hotmail.ch>
2016-05-12 19:17:39 +0200

    Perry Heatermanager + HeaterManagerImpl

--- project.feukora.server/src/projekt/feukora/server/business/HeaterManager.java
+++ project.feukora.server/src/projekt/feukora/server/business/HeaterManager.java
@@ -1,5 +1,9 @@
 package projekt.feukora.server.business;
 
+import java.util.List;
+
+import projekt.feukora.server.model.Heater;
+
 /**
  * Interface to handle heater entities.
  * 
@@ -9,5 +13,61 @@ package projekt.feukora.server.business;
  */
 
 public interface HeaterManager {
+	
+	/**
+	 * Save the given entity
+	 * 
+	 * @param entity
+	 * @throws Exception
+	 */
+	void saveHeater(Heater entity) throws Exception;
+
+	/**
+	 * Update the given entity
+	 * 
+	 * @param entity
+	 * @return Heater
+	 * @throws Exception
+	 */
+	Heater updateHeater(Heater entity) throws Exception;
+
+	/**
+	 * Delete the given entity
+	 * 
+	 * @param entity
+	 * @throws Exception
+	 */
+	void deleteHeater(Heater entity) throws Exception;
+
+	/**
+	 * Delete the entity with this id
+	 * 
+	 * @param id
+	 * @throws Exception
+	 */
+	void deleteHeaterByHeaterid(Integer heaterid) throws Exception;
+
+	/**
+	 * Return the entity with this id
+	 * 
+	 * @param id
+	 * @return Heater
+	 */
+	Heater findHeaterByHeaterid(Integer heaterid);
+
+	/**
+	 * Return all entities
+	 * 
+	 * @return
+	 */
+	List<Heater> findAllHeaters();
+
+	/**
+	 * Return a list with heaters with this name
+	 * 
+	 * @param name
+	 * @return List of Heaters
+	 */
+	public List<Heater> findHeaterByName(String name);
 
 }
--- project.feukora.server/src/projekt/feukora/server/business/HeaterManagerImpl.java
+++ project.feukora.server/src/projekt/feukora/server/business/HeaterManagerImpl.java
@@ -1,5 +1,13 @@
 package projekt.feukora.server.business;
 
+import java.util.List;
+
+import projekt.feukora.server.model.Heater;
+import projekt.feukora.server.persister.CustomerPersister;
+import projekt.feukora.server.persister.CustomerPersisterImpl;
+import projekt.feukora.server.persister.HeaterPersister;
+import projekt.feukora.server.persister.HeaterPersisterImpl;
+
 /**
  * This Class implements the methods of the interface HeaterManager.
  * 
@@ -7,6 +15,51 @@ package projekt.feukora.server.business;
  * @version 1.0
  */
 
-public class HeaterManagerImpl {
+public class HeaterManagerImpl implements HeaterManager {
+	
+	private HeaterPersister heaterPersister = new HeaterPersisterImpl();
+
+	@Override
+	public void saveHeater(Heater entity) throws Exception {
+		if((Integer) entity.getHeaterid() == null){
+			heaterPersister.saveHeater(entity);
+		}else{
+			throw new Exception("Der Heater (id = "+ entity.getHeaterid() + ") ist bereits vorhanden");
+		}
+		
+	}
+
+	@Override
+	public Heater updateHeater(Heater entity) throws Exception {
+		return heaterPersister.updateHeater(entity);
+		
+	}
+
+	@Override
+	public void deleteHeater(Heater entity) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteHeaterByHeaterid(Integer heaterid) throws Exception {
+		heaterPersister.deleteHeaterByHeaterid(heaterid);
+		
+	}
+
+	@Override
+	public Heater findHeaterByHeaterid(Integer heaterid) {
+		return heaterPersister.findHeaterByHeaterid(heaterid);
+	}
+
+	@Override
+	public List<Heater> findAllHeaters() {
+		return heaterPersister.findAllHeaters();
+	}
+
+	@Override
+	public List<Heater> findHeaterByName(String name) {
+		return heaterPersister.findHeaterByName(name);
+	}
 
 }
