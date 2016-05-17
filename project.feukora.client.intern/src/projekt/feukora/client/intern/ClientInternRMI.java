b0d3611d2e990e4034393d985f75b790a0fea4d0
Feukora <sandro-fasser@bluewin.ch>
2016-05-17 16:02:40 +0200

    Sandro Fasser RMIServer und Client

---
+++ project.feukora.client.intern/src/projekt/feukora/client/intern/ClientInternRMI.java
@@ -0,0 +1,76 @@
+package projekt.feukora.client.intern;
+
+import java.rmi.Naming;
+import projekt.feukora.server.rmi.AppointmentRMI;
+import projekt.feukora.server.rmi.BlowerRMI;
+import projekt.feukora.server.rmi.CompanyRMI;
+import projekt.feukora.server.rmi.CustomerRMI;
+import projekt.feukora.server.rmi.HeaterRMI;
+import projekt.feukora.server.rmi.MeasuringresultRMI;
+import projekt.feukora.server.rmi.RapportRMI;
+import projekt.feukora.server.rmi.UserRMI;
+
+
+/**
+ * Diese Klasse stellt das Userinterface fuer die Modulverwaltung via RMI zur
+ * Verfuegung
+ * This Class 
+ * 
+ * @version 1.0
+ * @author Sandro Fasser
+ *
+ */
+public class ClientInternRMI {
+	
+	CustomerRMI customerRMI;
+	CompanyRMI companyRMI;
+	AppointmentRMI appointmentRMI;
+	BlowerRMI blowerRMI;
+	HeaterRMI heaterRMI;
+	MeasuringresultRMI measuringresultRMI;
+	RapportRMI rapportRMI;
+	UserRMI userRMI;
+
+	
+	public static void main(String[] args) {
+		System.out
+				.println("======================================================================");
+		try {
+			// Init Application over RMI
+			ClientInternRMI feukora = new ClientInternRMI();
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		}
+	}
+	
+	/**
+	 * Instantiates a new modulverwaltung client rmi.
+	 *
+	 * @throws Exception
+	 */
+	public ClientInternRMI() throws Exception {
+
+
+		// init rmi connection
+		String url = "rmi://localhost:9009/";
+		String customerRMIName = "customerRMI";
+		String companyRMIName = "companyRMI";
+		String appointmentRMIName = "appointmentRMI";
+		String blowerRMIName = "blowerRMI";
+		String heaterRMIName = "heaterRMI";
+		String measuringresultRMIName = "measuringresultRMI";
+		String rapportRMIName = "rapportRMI";
+		String userRMIName = "userRMI";
+		this.customerRMI = (CustomerRMI) Naming.lookup(url + customerRMIName);
+		this.companyRMI = (CompanyRMI) Naming.lookup(url + companyRMIName);
+		this.appointmentRMI = (AppointmentRMI) Naming.lookup(url + appointmentRMIName);
+		this.blowerRMI = (BlowerRMI) Naming.lookup(url + blowerRMIName);
+		this.heaterRMI = (HeaterRMI) Naming.lookup(url + heaterRMIName);
+		this.measuringresultRMI = (MeasuringresultRMI) Naming.lookup(url + measuringresultRMIName);
+		this.rapportRMI = (RapportRMI) Naming.lookup(url + rapportRMIName);
+		this.userRMI = (UserRMI) Naming.lookup(url + userRMIName);
+		
+	}
+	
+}
