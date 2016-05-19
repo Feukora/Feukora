31f9554841b866f5907a334192263fff59775a5e
Perry1234 <perry_00@hotmail.ch>
2016-05-19 14:34:48 +0200

    Customerfunction Business & RMI - Perry

---
+++ project.feukora.server/src/projekt/feukora/server/business/CustomerfunctionManagerImpl.java
@@ -0,0 +1,61 @@
+package projekt.feukora.server.business;
+
+import java.util.List;
+
+import projekt.feukora.server.model.Customerfunction;
+import projekt.feukora.server.persister.CustomerfunctionPersister;
+import projekt.feukora.server.persister.CustomerfunctionPersisterImpl;
+
+/**
+ * This Class implements the methods of the interface CustomerfunctionManager.
+ * @author Patrick
+ * @version 1.0
+ */
+
+public class CustomerfunctionManagerImpl implements CustomerfunctionManager {
+
+	private CustomerfunctionPersister customerfunctionPersister = new CustomerfunctionPersisterImpl();
+	
+	@Override
+	public void saveCustomerfunction(Customerfunction entity) throws Exception {
+		if((Integer) entity.getCustomerfunctionid() == null){
+			customerfunctionPersister.saveCustomerfunction(entity);
+		}else{
+			throw new Exception("Die Kundenfunktion (id = "+ entity.getCustomerfunctionid() + ") ist bereits vorhanden");
+		}
+		
+	}
+
+	@Override
+	public Customerfunction updateCustomerfunction(Customerfunction entity) throws Exception {
+		return customerfunctionPersister.updateCustomerfunction(entity);
+	}
+
+	@Override
+	public void deleteCustomerfunction(Customerfunction entity) throws Exception {
+		customerfunctionPersister.deleteCustomerfunction(entity);
+		
+	}
+
+	@Override
+	public void deleteCustomerfunctionByCustomerfunctionid(Integer customerfunctionid) throws Exception {
+		customerfunctionPersister.deleteCustomerfunctionByCustomerfunctionid(customerfunctionid);
+		
+	}
+
+	@Override
+	public Customerfunction findCustomerfunctionByCustomerfunctionid(Integer customerfunctionid) {
+		return customerfunctionPersister.findCustomerfunctionByCustomerfunctionid(customerfunctionid);
+	}
+
+	@Override
+	public List<Customerfunction> findAllCustomerfunction() {
+		return customerfunctionPersister.findAllCustomerfunction();
+	}
+
+	@Override
+	public Customerfunction findCustomerfunctionByName(String name) {
+		return customerfunctionPersister.findCustomerfunctionByName(name);
+	}
+
+}
---
+++ project.feukora.server/src/projekt/feukora/server/rmi/CustomerfunctionRMIImpl.java
@@ -0,0 +1,60 @@
+package projekt.feukora.server.rmi;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+import projekt.feukora.server.business.CustomerfunctionManager;
+import projekt.feukora.server.business.CustomerfunctionManagerImpl;
+import projekt.feukora.server.model.Customerfunction;
+
+/**
+ *  This Class implements the methods of the interface CustomerfunctionRMI
+ * @author Patrick
+ * @version 1.0
+ */
+
+public class CustomerfunctionRMIImpl extends UnicastRemoteObject implements CustomerfunctionRMI {
+
+	private static final long serialVersionUID = 1266986892096541978L;
+
+	private CustomerfunctionManager customerfunctionManager;
+
+	public CustomerfunctionRMIImpl() throws RemoteException {
+		customerfunctionManager = new CustomerfunctionManagerImpl();
+	}
+
+	@Override
+	public void saveCustomerfunction(Customerfunction entity) throws RemoteException, Exception {
+		customerfunctionManager.saveCustomerfunction(entity);	
+	}
+
+	@Override
+	public Customerfunction updateCustomerfunction(Customerfunction entity) throws RemoteException, Exception {
+		return customerfunctionManager.updateCustomerfunction(entity);
+	}
+
+	@Override
+	public void deleteCustomerfunction(Customerfunction entity) throws RemoteException, Exception {
+		customerfunctionManager.deleteCustomerfunction(entity);
+	}
+
+	@Override
+	public void deleteCustomerfunctionByCustomerfunctionid(Integer customerfunctionid) throws RemoteException, Exception {
+		customerfunctionManager.deleteCustomerfunctionByCustomerfunctionid(customerfunctionid);	
+	}
+
+	@Override
+	public Customerfunction findCustomerfunctionByCustomerfunctionid(Integer customerfunctionid) throws RemoteException {
+		return customerfunctionManager.findCustomerfunctionByCustomerfunctionid(customerfunctionid);
+	}
+
+	@Override
+	public List<Customerfunction> findAllCustomerfunction() throws RemoteException {
+		return customerfunctionManager.findAllCustomerfunction();
+	}
+
+	@Override
+	public Customerfunction findCustomerfunctionByName(String name) throws RemoteException {
+		return customerfunctionManager.findCustomerfunctionByName(name);
+		}
+}
