5172b202ce45090d932d77c4780961c9113fcf93
Perry1234 <perry_00@hotmail.ch>
2016-05-21 16:20:19 +0200

    Rappor Controller Main - Buttons & Methods

--- project.feukora.client.intern/src/rapport/controller/ControllerMain.java
+++ project.feukora.client.intern/src/rapport/controller/ControllerMain.java
@@ -3,14 +3,53 @@ package rapport.controller;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import projekt.feukora.client.intern.ClientInternRMI;
 
 public class ControllerMain implements Initializable{
 
+	private Button rapportstartweiter;
+	private Button waermeerzeugerzurueck;
+	private Button waermeerzeugerweiter;
+	private Button messergebnissezurueck;
+	private Button messergebnisseweiter;
+	private Button beurteilungzurueck;
+	private Button beurteilungspeichern;
+	
+	
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		
 		
 	}
 
+	@FXML
+    void Actionrapportstartweiter(ActionEvent event) {
+    }
+	
+	void Actionwaermeerzeugerzurueck(ActionEvent event) {
+    }
+	
+	void Actionwaermeerzeugerweiter(ActionEvent event) {
+    }
+	
+	void Actionmessergebnissezurueck(ActionEvent event) {
+    }
+	
+	void Actionmessergebnisseweiter(ActionEvent event) {
+    }
+	
+	void Actionbeurteilungzurueck(ActionEvent event) {
+    }
+	
+	void Actionbeurteilungspeichern(ActionEvent event) {
+    }
+	
+	
+	
 }
