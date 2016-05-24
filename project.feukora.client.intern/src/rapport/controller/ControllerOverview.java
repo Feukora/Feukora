50a90ce8a7846342589032a559372161bd4f0aa4
Allan <Allan@AllanPC>
2016-05-24 16:52:11 +0200

    Allan Rapport Overview ID's vergeben // Reihenfolge angepasst

--- project.feukora.client.intern/src/rapport/controller/ControllerOverview.java
+++ project.feukora.client.intern/src/rapport/controller/ControllerOverview.java
@@ -3,18 +3,34 @@ package rapport.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 
 public class ControllerOverview {
-
+	
     @FXML
     private Button overviewDeleteRapport;
-    
+
+    @FXML
+    private Button overviewUpdateRapport;
+
     @FXML
     private Button overviewRefreshRapport;
     
     @FXML
-    private Button overviewUpdateRapport;
-        
+    private TableColumn<?, ?> overviewTableControlltype;
+
+    @FXML
+    private TableColumn<?, ?> overviewTableResult;
+
+    @FXML
+    private TableColumn<?, ?> overviewTableCustomer;
+
+    @FXML
+    private TableColumn<?, ?> overviewTableInspector;
+
+    @FXML
+    private TableColumn<?, ?> overviewTableDate;
+
     @FXML
     void ActionOverviewDeleteRapport(ActionEvent event) {
 
@@ -24,9 +40,10 @@ public class ControllerOverview {
     void ActionOverviewRefreshRapport(ActionEvent event) {
 
     }
-    
+
     @FXML
     void ActionOverviewUpdateRapport(ActionEvent event) {
 
     }
+
 }
