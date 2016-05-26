9659cadbd6d170b4f597d289eb16c0b27b0051d9
Feukora <sandro-fasser@bluewin.ch>
2016-05-26 15:19:07 +0200

    Sandro Faser login

--- project.feukora.client.intern/src/login/controller/ControllerLogin.java
+++ project.feukora.client.intern/src/login/controller/ControllerLogin.java
@@ -18,115 +18,130 @@ import javafx.scene.layout.BorderPane;
 import projekt.feukora.client.intern.ClientInternRMI;
 
 public class ControllerLogin {
-	
+
 	private static final Logger logger = Logger
 			.getLogger(ControllerLogin.class);
 
-    @FXML
-    private TextField usernameLoginTextfield;
+	@FXML
+	private TextField usernameLoginTextfield;
 
-    @FXML
-    private PasswordField passwordLoginTextfield;
+	@FXML
+	private PasswordField passwordLoginTextfield;
 
-    @FXML
-    private Button loginLogin;
+	@FXML
+	private Button loginLogin;
 
-    @FXML
-    private Button passwordForgetLogin;
+	@FXML
+	private Button passwordForgetLogin;
 
-    @FXML
-    void ActionUsernameLoginTextfield(ActionEvent event) {
+	@FXML
+	void ActionUsernameLoginTextfield(ActionEvent event) {
 
-    }
+	}
 
-    @FXML
-    void ActionPasswordLoginTextfield(ActionEvent event) {
-    	BorderPane pane = new BorderPane();
-    	
-		String username = usernameLoginTextfield.getText();
-		String password = passwordLoginTextfield.getText();
-
-    	try {
-    		ClientInternRMI feukora = new ClientInternRMI();
-    		Boolean successfull = feukora.login(username, password);
-    		
-    		if(successfull == true){
-    			String role = feukora.authentication(username);
-    			Context.setRole(role);
-    			if(role.equals("Administrator")) {
-    				pane = FXMLLoader.load(getClass().getClassLoader().getResource("application/MainViewAdministrator.fxml"));
-    			} else if (role.equals("Feuerungskontrolleur")) {
-    				pane = FXMLLoader.load(getClass().getClassLoader().getResource("application/MainViewInspector.fxml"));
-    			} else if (role.equals("Sachbearbeiter")) {
-    				pane = FXMLLoader.load(getClass().getClassLoader().getResource("application/MainViewAssistant.fxml"));
-    			}
-    			
-    		}else{
-    			pane = FXMLLoader.load(getClass().getClassLoader().getResource("login/view/loginPane.fxml"));
-    			usernameLoginTextfield.clear();
-    			passwordLoginTextfield.clear();
-    			String titleBar = "Uups!! Passwort falsch";
-    			String headerMessage = "Passwort oder Benutzename ist falsch";
-    			String infoMessage = "-";
-    			Alert alert = new Alert(AlertType.INFORMATION);
-    	        alert.setTitle(titleBar);
-    	        alert.setHeaderText(headerMessage);
-    	        alert.setContentText(infoMessage);
-    	        alert.showAndWait();
-    		}
-    		
-			} catch (Exception e) {
-				logger.error("Login fehlgeschlagen\'",
-						e);
-		}	
-    	
-    	loginLogin.getScene().setRoot(pane);
-    }
-
-    @FXML
-    void ActionLoginLogin(ActionEvent event) {
+	@FXML
+	void ActionPasswordLoginTextfield(ActionEvent event) {
 		BorderPane pane = new BorderPane();
-    	
+
 		String username = usernameLoginTextfield.getText();
 		String password = passwordLoginTextfield.getText();
-		
-    	try {
-    		ClientInternRMI feukora = new ClientInternRMI();
-    		Boolean successfull = feukora.login(username, password);
-    		
-    		if(successfull == true){
-    			pane = FXMLLoader.load(getClass().getClassLoader().getResource("application/MainViewAdministrator.fxml"));
-    		}else{
-    			pane = FXMLLoader.load(getClass().getClassLoader().getResource("login/view/loginPane.fxml"));
-    			usernameLoginTextfield.clear();
-    			passwordLoginTextfield.clear();
-    			String titleBar = "Uups!! Passwort falsch";
-    			String headerMessage = "Passwort oder Benutzename ist falsch";
-    			String infoMessage = "-";
-    			Alert alert = new Alert(AlertType.INFORMATION);
-    	        alert.setTitle(titleBar);
-    	        alert.setHeaderText(headerMessage);
-    	        alert.setContentText(infoMessage);
-    	        alert.showAndWait();
-    		}
-    		
-			} catch (Exception e) {
-				logger.error("Login fehlgeschlagen\'",
-						e);
+
+		try {
+			ClientInternRMI feukora = new ClientInternRMI();
+			if(username.isEmpty() == true || password.isEmpty() == true){
+
+				Boolean successfull = feukora.login(username, password);
+
+				if(successfull == true){
+					String role = feukora.authentication(username);
+					Context.setRole(role);
+					if(role.equals("Administrator")) {
+						pane = FXMLLoader.load(getClass().getClassLoader().getResource("application/MainViewAdministrator.fxml"));
+					} else if (role.equals("Feuerungskontrolleur")) {
+						pane = FXMLLoader.load(getClass().getClassLoader().getResource("application/MainViewInspector.fxml"));
+					} else if (role.equals("Sachbearbeiter")) {
+						pane = FXMLLoader.load(getClass().getClassLoader().getResource("application/MainViewAssistant.fxml"));
+					}
+
+				}else{
+					pane = FXMLLoader.load(getClass().getClassLoader().getResource("login/view/loginPane.fxml"));
+					usernameLoginTextfield.clear();
+					passwordLoginTextfield.clear();
+					String titleBar = "Uups!! Passwort falsch";
+					String headerMessage = "Passwort oder Benutzename ist falsch";
+					String infoMessage = "-";
+					Alert alert = new Alert(AlertType.INFORMATION);
+					alert.setTitle(titleBar);
+					alert.setHeaderText(headerMessage);
+					alert.setContentText(infoMessage);
+					alert.showAndWait();
+				}
+			}else{
+				pane = FXMLLoader.load(getClass().getClassLoader().getResource("login/view/loginPane.fxml"));
+				usernameLoginTextfield.clear();
+				passwordLoginTextfield.clear();
+				String titleBar = "Nichts eingegeben";
+				String headerMessage = "Passwort und Benutzername eingeben";
+				String infoMessage = "-";
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle(titleBar);
+				alert.setHeaderText(headerMessage);
+				alert.setContentText(infoMessage);
+				alert.showAndWait();
+			}
+		} catch (Exception e) {
+			logger.error("Login fehlgeschlagen\'",
+					e);
+		}
+
+
+		loginLogin.getScene().setRoot(pane);
+	}
+
+	@FXML
+	void ActionLoginLogin(ActionEvent event) {
+		BorderPane pane = new BorderPane();
+
+		String username = usernameLoginTextfield.getText();
+		String password = passwordLoginTextfield.getText();
+
+		try {
+			ClientInternRMI feukora = new ClientInternRMI();
+			Boolean successfull = feukora.login(username, password);
+
+			if(successfull == true){
+				pane = FXMLLoader.load(getClass().getClassLoader().getResource("application/MainViewAdministrator.fxml"));
+			}else{
+				pane = FXMLLoader.load(getClass().getClassLoader().getResource("login/view/loginPane.fxml"));
+				usernameLoginTextfield.clear();
+				passwordLoginTextfield.clear();
+				String titleBar = "Uups!! Passwort falsch";
+				String headerMessage = "Passwort oder Benutzename ist falsch";
+				String infoMessage = "-";
+				Alert alert = new Alert(AlertType.INFORMATION);
+				alert.setTitle(titleBar);
+				alert.setHeaderText(headerMessage);
+				alert.setContentText(infoMessage);
+				alert.showAndWait();
+			}
+
+		} catch (Exception e) {
+			logger.error("Login fehlgeschlagen\'",
+					e);
 		}	
-    	
-    	loginLogin.getScene().setRoot(pane);
-    }
-    
-    @FXML
-    void ActionPasswordForgetLogin(ActionEvent event) {
+
+		loginLogin.getScene().setRoot(pane);
+	}
+
+	@FXML
+	void ActionPasswordForgetLogin(ActionEvent event) {
 		String titleBar = "Passwort vergessen?";
 		String headerMessage = "Melden Sie sich fürs Zurücksetzen beim Administrator:";
 		String infoMessage = "Telefon: 081 407 11 11 Email: admin@test.ch";
 		Alert alert = new Alert(AlertType.INFORMATION);
-        alert.setTitle(titleBar);
-        alert.setHeaderText(headerMessage);
-        alert.setContentText(infoMessage);
-        alert.showAndWait();
-    }
+		alert.setTitle(titleBar);
+		alert.setHeaderText(headerMessage);
+		alert.setContentText(infoMessage);
+		alert.showAndWait();
+	}
 }
