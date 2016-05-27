6786b80e9550cb36c251875fd7ab9875192bbfd0
Allan <Allan@AllanPC>
2016-05-27 10:46:43 +0200

    Allan Feinheiten

--- project.feukora.client.intern/src/application/ControllerViewAssistant.java
+++ project.feukora.client.intern/src/application/ControllerViewAssistant.java
@@ -237,7 +237,7 @@ public class ControllerViewAssistant {
 		 try {
 			 pane = FXMLLoader.load(getClass().getClassLoader().getResource("calendar/view/calendarPane.fxml"));
 
-			 mainRoot.setCenter(pane);
+			 mainRoot.setTop(pane);
 
 		 } catch (Exception e) {
 			 logger.error("Aktion konnte nicht durchgeführt werden\'",
